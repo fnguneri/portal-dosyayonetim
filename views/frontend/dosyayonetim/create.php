d2092424bb253f121e98b4b1333541b69f7f3164
fnguneri <45860464+fnguneri@users.noreply.github.com>
2019-01-03 20:18:05 +0300

    Add files via upload

---
+++ views/frontend/dosyayonetim/create.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model kouosl\dosyayonetim\models\Dosyayonetim */
+
+$this->title = 'Create Dosyayonetim';
+$this->params['breadcrumbs'][] = ['label' => 'Dosyayonetims', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="dosyayonetim-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
